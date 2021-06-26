434482070aa7b6089d1080327399b6b1e7fd6485
Marina Joranhezon <marina.joranhezon@gmail.com>
2021-06-26 20:08:17 -0300

    refactor: make SearchRestaurantsService#search_partial_string more optimal

--- app/services/search_restaurants_service.rb
+++ app/services/search_restaurants_service.rb
@@ -3,18 +3,18 @@
 class SearchRestaurantsService
   RESTAURANT_CSV_PATH = 'docs/restaurants.csv'
   CUISINE_CSV_PATH = 'docs/cuisines.csv'
-  RESTAURANT_DATA = ExtractRestaurantDataService.new.call(RESTAURANT_CSV_PATH).freeze
   CUISINE_DATA = ExtractRestaurantDataService.new.call(CUISINE_CSV_PATH).freeze
 
   def call(name, customer_rating, distance, price, cuisine)
-    response = search_partial_string(:name, name, RESTAURANT_DATA) if name.present?
+    restaurant_data = ExtractRestaurantDataService.new.call(RESTAURANT_CSV_PATH)
+    restaurant_data = search_partial_string(:name, name, restaurant_data) if name.present?
   end
 
   private
 
   def search_partial_string(key, content, data)
-    data.map do |item|
-      item if item[key].downcase.include? content.downcase
-    end.compact
+    data.select do |item|
+      item[key].downcase.include? content.downcase
+    end
   end
 end
